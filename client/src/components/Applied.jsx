8210ec016a0e2e1029aa502a22513a77264f0de2
Daniel Flores <danielflores125@gmail.com>
2020-12-09 10:38:57 -0800

    Basic implementation of Applied form completed, still needs on click submit to server

--- client/src/components/Applied.jsx
+++ client/src/components/Applied.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import styled from 'styled-components';
 import Modal from 'react-modal';
 
-
 const Wrap = styled.div`
 border-style: solid;
 border-color: red;
@@ -17,37 +16,53 @@ background-color: #fff;
 border-radius: 5px;
 border:4px solid transparent;
 box-shadow: 0px 0px 7px rgba(25,4,69,0.1);
+outline: 0;
+
+`;
+
+const FormWrap = styled.form`
+display:grid;
+grid-template-columns: 100%;
+grid-template-rows: 100%;
 `;
 
 const customStyles = {
-  content : {
-    top                   : '50%',
-    left                  : '50%',
-    right                 : 'auto',
-    bottom                : 'auto',
-    marginRight           : '-50%',
-    transform             : 'translate(-50%, -50%)'
-  }
+  content: {
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)',
+    height: '300px',
+    width: '300px',
+  },
 };
 
-
-
 class Applied extends React.Component {
-constructor(props){
-  super(props)
-this.state = {visible: false}
+  constructor(props) {
+    super(props);
+    this.state = {
+      visible: false, company: '', date: '', position: '',
+    };
 
-this.showModal = this.showModal.bind(this);
-}
+    this.showModal = this.showModal.bind(this);
+    this.hideModal = this.hideModal.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
 
-showModal() {
-  this.setState({ visible: true });
-}
+  showModal() {
+    this.setState({ visible: true });
+  }
 
-hideModal() {
-  this.setState({ visible: false });
-}
+  hideModal() {
+    this.setState({ visible: false });
+  }
 
+  handleChange(event) {
+    const { name } = event.target;
+    this.setState({ [name]: event.target.value });
+  }
 
   render() {
     const appliedCompanies = this.props.companies.map((company) => (
@@ -56,11 +71,29 @@ hideModal() {
         {company.company}
       </div>
     ));
+
+    const {company, date, position} = this.state;
     return (
       <Wrap>
         Applied
         <Modal isOpen={this.state.visible} style={customStyles}>
-          <div>helllo</div>
+          <button onClick={this.hideModal}>X</button>
+          <FormWrap onSubmit={this.handleSubmit}>
+            <label>
+              Company Name:
+              <input type="text" name="company" value={company} onChange={this.handleChange} />
+            </label>
+            <label>
+              Date applied:
+              <input type="text" name="date" value={date} onChange={this.handleChange} />
+            </label>
+            <label>
+              Position:
+              <input type="text" name="position" value={position} onChange={this.handleChange} />
+            </label>
+            <input type="submit" value="Submit" />
+          </FormWrap>
+
         </Modal>
         <AddButton onClick={this.showModal}> + </AddButton>
         {appliedCompanies}
